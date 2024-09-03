61bbf8f356c6f6d8de8e74e93661bf6435a9c17e
Alvinkimutai <alvinchepkiyeng@gmail.com>
2024-09-03 08:54:39 +0300

    practices on the for loop and while loop

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,42 @@
 
 def happy_new_year():
     # code goes here!
+    n = 11
+    while n > 1:
+        n-= 1
+        print(n)
+    if n==1 :
+        print("Happy New Year!")
     pass
+
+happy_new_year()
 
 def square_integers(int_list):
     # code goes here!
-    pass
 
+
+    # squared_integers= list()
+    # for elements in int_list:
+    #     integers_squared = elements**2
+    #     squared_integers.append(integers_squared)
+
+    # return squared_integers
+    sqr = [element**2 for element in int_list]
+    return sqr
+    pass
+print(square_integers([1,3,5,7,9]))
 def fizzbuzz():
     # code goes here!
+    for num in range(1,101,1):
+        if num%3 ==0 and num%5 !=0:
+            print("Fizz")
+        elif num%3 != 0 and num%5 == 0:
+            print("Buzz")
+        elif num%3 == 0 and num%5 == 0:
+            print("FizzBuzz")
+        else:
+            print(num)
+
     pass
+print(fizzbuzz())
+
